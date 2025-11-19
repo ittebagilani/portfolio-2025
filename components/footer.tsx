d889cd5b2f6bbddfe5aae31d226aa2c23ec2ffcf
Itteba Gilani <itteba1@gmail.com>
2025-11-19 09:41:53 -0500

    nov19.2

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,6 @@ export default function Footer() {
   return (
     <footer className="w-full border-t border-neutral-800 text-black py-6 lowercase mb-10">
       <div className="max-w-6xl mx-auto px-4 flex flex-col md:flex-row items-start md:items-center justify-between gap-6">
-
         {/* Left Section */}
         <div className="flex flex-col">
           <a href="#" className="text-lg font-medium hover:underline">
@@ -29,10 +28,19 @@ export default function Footer() {
           >
             say hi
           </a>
-
-          <p className="mt-3 font-medium">Resume</p>
+          <a
+            href="/files/resume.pdf"
+            target="_blank"
+            rel="noopener noreferrer"
+            className="mt-3 font-medium hover:underline"
+          >
+            Resume
+          </a>{" "}
           <div className="flex gap-3">
-            <a href="https://www.linkedin.com/in/itteba-gilani/" className="hover:underline">
+            <a
+              href="https://www.linkedin.com/in/itteba-gilani/"
+              className="hover:underline"
+            >
               Linkedin
             </a>
             <a href="https://x.com/iittebagilanii" className="hover:underline">
@@ -40,7 +48,6 @@ export default function Footer() {
             </a>
           </div>
         </div>
-
       </div>
     </footer>
   );
